51d1752b9b5f78f9bcb9d285e1937b0b674f2867
Dyaaki <94301920+Dyaaki@users.noreply.github.com>
2021-11-14 16:17:14 +0100

    test3

--- setcal.c
+++ setcal.c
@@ -2,18 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ELEM_LEN 30
+
+char *element;
+
 typedef struct
 {
 	int line;
-	char **elements;
+	char **elem_arr;
+	int length;
 } set_t;
 
 typedef struct
 {
+  char *el1;
+  char *el2;
+} elpair_t;
+
+typedef struct
+{
 	int line;
-	char ***elements;
+	elpair_t **elem_arr;
+	int length;
 } rel_t;
 
+int set_create(set_t *s)
+{
+  //alokace cele mnoziny
+  s->elem_arr = malloc(s->length * sizeof(char *));
+  if (s->elem_arr == NULL)
+      return 1;
+  //alokace jednotlivych prvku
+  for (int i = 0; i < s->length; i++) {
+    s->elem_arr[i] = malloc(30 * sizeof(char));
+    if (s->elem_arr == NULL)
+      return 1;
+  }
+
+  return 0;
+}
+
+
+int main()
+{
+  char *element = malloc(30 * sizeof(char));
+
+
+  return EXIT_SUCCESS;
+}
 
 // set_t { [ab], [cd] }
 
